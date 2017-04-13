0f210468c7e847beb08d5679077c6a5b63f87bba
kjurek <kjurek123@gmail.com>
2017-04-13 16:18:02 +0200

    Moved main to TotalActivationTool.py and removed unused class members

--- TotalActivationTool.py
+++ TotalActivationTool.py
@@ -1,33 +1,40 @@
 import scipy.io as sio
 import numpy as np
 import logging
+
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
 
 class TotalActivationTool(object):
     def __init__(self):
         # Method_time: 'B', 'S' or 'W'
         # Method_space: 'S', 'T', None
         # HRF: 'bold', 'spmhrf'
-        self.config = {'Method_time' : 'B',
-                       'Method_space' : None,
-                       'HRF' : 'bold',
+        self.config = {'Method_time': 'B',
+                       'Method_space': None,
+                       'HRF': 'bold',
                        'Detrend': True,
-                       'Standardize' : True,
-                       'Highpass' : 0.01,
-                       'Lowpass' : None,
-                       'TR' : 2,
-                       'Lambda' : 1/0.8095}
-        self.voxels = None
+                       'Standardize': True,
+                       'Highpass': 0.01,
+                       'Lowpass': None,
+                       'TR': 2,
+                       'Lambda': 1 / 0.8095}
+        self.data = None
+        self.atlas = None
 
-    def load(self, data_path, atlas_path, config):
-        self.config = config
-        data = sio.loadmat(data_path)['data']
-        atlas = sio.loadmat(atlas_path)['atlas']
-        self.data_shape = data.shape
-        self.dimension = len(data.shape)
-        self.voxels = data[np.nonzero(atlas * np.ndarray.sum(data, axis=self.dimension - 1))]
-        logging.debug('Dimension={}'.format(self.dimension))
-        logging.debug('Voxels.shape={}'.format(self.voxels.shape))
+    def load_matlab_data(self, d, a):
+        '''
+        Loads data and atlas in matlab format
+
+        Inputs:
+        d : data (Matlab file with 4D matrix 'data' variable)
+        a : atlas (Matlab file with 3D matrix 'atlas' variable)
+        '''
+        data = sio.loadmat(d)['data']
+        self.atlas = sio.loadmat(a)['atlas']
+        self.data = data[np.nonzero(self.atlas * np.ndarray.sum(data, axis=len(data.shape) - 1))]
+        logging.debug('self.data.shape={}'.format(self.data.shape))
+        logging.debug('self.atlas.shape={}'.format(self.atlas.shape))
 
     def load_nifti_data(self, d, a):
         '''
@@ -41,29 +48,30 @@ class TotalActivationTool(object):
         import nibabel as nib
         from nilearn.input_data import NiftiMasker
 
-        self.atlas_masker = NiftiMasker(mask_strategy='background',
-                           memory="nilearn_cache", memory_level=2,
-                           standardize=False,
-                           detrend=False)
+        atlas_masker = NiftiMasker(mask_strategy='background',
+                                   memory="nilearn_cache", memory_level=2,
+                                   standardize=False,
+                                   detrend=False)
 
-        self.data_masker = NiftiMasker(mask_strategy='epi',
-                           memory="nilearn_cache", memory_level=2,
-                           standardize=self.config['Standardize'],
-                           detrend=self.config['Detrend'],
-                           high_pass = self.config['Highpass'],
-                           low_pass = self.config['Lowpass'],
-                           t_r = self.config['TR'])
+        data_masker = NiftiMasker(mask_strategy='epi',
+                                  memory="nilearn_cache", memory_level=2,
+                                  standardize=self.config['Standardize'],
+                                  detrend=self.config['Detrend'],
+                                  high_pass=self.config['Highpass'],
+                                  low_pass=self.config['Lowpass'],
+                                  t_r=self.config['TR'])
 
-        self.atlas_masker.fit(a)
-        self.data_masker.fit(d)
-        x1 = self.data_masker.mask_img_.get_data()
-        x2 = self.atlas_masker.mask_img_.get_data()
+        atlas_masker.fit(a)
+        data_masker.fit(d)
+        x1 = data_masker.mask_img_.get_data()
+        x2 = atlas_masker.mask_img_.get_data()
         x1 *= x2
         x2 *= x1
-        self.data = self.data_masker.transform(d)
-        self.atlas = self.atlas_masker.transform(a)
-        self.data_shape = self.data.shape
-        self.dimension = len(self.data.shape)
-        self.n_voxels = self.data.shape[1]
-        logging.debug('Dimension={}'.format(self.dimension))
-        logging.debug('Voxels.shape={}'.format(self.n_voxels))
+        self.data = data_masker.transform(d)
+        self.atlas = atlas_masker.transform(a)
+        logging.debug('self.data.shape={}'.format(self.data.shape))
+        logging.debug('self.atlas.shape={}'.format(self.atlas.shape))
+
+
+if __name__ == '__main__':
+    ta = TotalActivationTool()
